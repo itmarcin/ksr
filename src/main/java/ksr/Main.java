cbdab91892525e3df329f0f873f15be37b2b1255
Marcin Lewandowski <marcinlewandowski1999@gmail.com>
2022-03-09 23:26:54 +0100

    Add fasterxml jackson dependency
    We need to check if jackson is working for SGML files in our project

--- src/main/java/ksr/Main.java
+++ src/main/java/ksr/Main.java
@@ -1,7 +1,25 @@
 package ksr;
 
-public class Main {
-    public static void main(String[] args) {
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import java.io.*;
 
+public class Main {
+    public static <File> void main(String[] args) {
+
+//        File file = new File("simple_bean.xml");
+//        XmlMapper xmlMapper = new XmlMapper();
+//        String xml = inputStreamToString(new FileInputStream(file));
+//        SimpleBean value = xmlMapper.readValue(xml, SimpleBean.class);
     }
+
+//    public static String inputStreamToString(InputStream is) throws IOException {
+//        StringBuilder sb = new StringBuilder();
+//        String line;
+//        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+//        while ((line = br.readLine()) != null) {
+//            sb.append(line);
+//        }
+//        br.close();
+//        return sb.toString();
+//    }
 }
